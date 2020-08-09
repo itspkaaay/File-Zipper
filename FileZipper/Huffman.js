03e1536d38328fefb7d2a70f2f7a561fb688f992
Parikshit Sachdeva <65595301+itspkaaay@users.noreply.github.com>
2020-08-10 01:25:20 +0530

    Add files via upload

--- FileZipper/Huffman.js
+++ FileZipper/Huffman.js
@@ -9,22 +9,28 @@ class HuffmanEncoder{
     mappings contain character to binary mapping */
 
     /// To get a stringified huffman tree
+
+
+
+
     huffmanString(huffmanTreeNode)
     {
-        //console.log(huffmanTreeNode);
         if(huffmanTreeNode===undefined)
         {
             return;
         }
         if(typeof(huffmanTreeNode[1])==='string')
         {
-            return "^"+huffmanTreeNode[1];
+            return "^"+huffmanTreeNode[1];    
         }
         let lstr= "0"+this.huffmanString(huffmanTreeNode[1][0]);
         let rstr= "1"+this.huffmanString(huffmanTreeNode[1][1]);
 
         return lstr+rstr;
     }
+
+
+
     /// get binary mappings of each character in string
     getBinaryCodes(huffmanTreeNode,path)
     {
@@ -42,6 +48,11 @@ class HuffmanEncoder{
         this.getBinaryCodes(huffmanTreeNode[1][1],path+"1");
         
     }
+
+
+
+
+
     /// encoding method
     encode(data)
     {
@@ -65,6 +76,7 @@ class HuffmanEncoder{
             this.heap.insert([-mp[key],key]);
         }
 
+
         while(this.heap.size()>1)
         {
             const node1= this.heap.remove_max();
@@ -78,37 +90,40 @@ class HuffmanEncoder{
         this.mappings={};
         this.getBinaryCodes(huffmanTree,"");
 
-        //default binary encoded string
+        //default binary encoded string @binaryEncodedString
         let binaryEncodedString="";
         for(let i=0;i<data.length;i++)
         {
             binaryEncodedString+= this.mappings[data[i]];
         }
 
-        /// create 8 bits buckets to get a unique character with its associated ascii values
 
+        /// get final characterised encoded string @characterisedString
         let rem= 8-(binaryEncodedString.length%8);
+
         let paddedstring="";
         for(let i=0;i<rem;i++)
         {
             paddedstring+="0";
         }
         binaryEncodedString+=paddedstring;
-        console.log("binary encoded string:", binaryEncodedString);
-
-        /// separating buckets of 8 and replacing with a character
         let characterisedString="";
         for(let i=0;i<binaryEncodedString.length;i+=8)
         {
             characterisedString+=String.fromCharCode(parseInt(binaryEncodedString.slice(i,i+8),2));
         }
 
-        const retString= this.huffmanString(huffmanTree).concat("\n",rem,"\n"+characterisedString);
-        const retInfo= "The compression ratio for this file is: "+ String(data.length/retString.length)
-        console.log(retInfo);
-        console.log(this.mappings);
-        this.decode(retString);
-        return retString;
+
+
+        const huffmanTreeString=this.huffmanString(huffmanTree);
+
+        const retString= huffmanTreeString.concat("\n",rem,"\n"+characterisedString);
+        console.log("input data size:",data.length);
+        console.log("output datasize:",retString.length);
+        const retInfo= "The compression ratio for this file is: "+ String(data.length/retString.length);
+        console.log([retString,retInfo]);
+        console.log(retString);
+        return [retString,retInfo];
 
     }
 
@@ -138,13 +153,32 @@ class HuffmanEncoder{
     // decode the encoded string
     decode(encoded_data)
     {
-        console.log("String to decode:");
-        console.log(encoded_data);
         encoded_data=encoded_data.split("\n");
-        let encoded_og= encoded_data[2];          /// actual string sans meta data
-        let htreeString= encoded_data[0];
-        let rem= encoded_data[1];
+        encoded_data[1]=encoded_data[0]+"\n"+encoded_data[1];
+        encoded_data.shift();
 
+        console.log("adjusting data-");
+        console.log(encoded_data);
+
+        if(encoded_data.length>3)
+        {
+            for(let i=3;i<encoded_data.length;i++)
+            {
+                encoded_data[2]+="\n"+encoded_data[i];
+            }
+
+            while(encoded_data.length>3)
+            {
+                encoded_data.pop();
+            }
+        }
+
+        let htreeString= encoded_data[0];
+        let rem= parseInt(encoded_data[1]);
+        let encoded_og="";
+
+        // taking into account line break formatting
+        encoded_og= encoded_data[2];  
         let binarised_og="";
 
         /// return binarised string to be parsed in the tree
@@ -156,8 +190,6 @@ class HuffmanEncoder{
         binarised_og=binarised_og.substr(0, binarised_og.length-rem);
         this.ind=0;
         let decoder=this.destringHuffman(htreeString);
-        console.log("decoder:", decoder);
-        console.log("string to decode:",binarised_og);
         let currentNode= decoder;
         let res="";
         for(let i=0;i<binarised_og.length;i++)
@@ -177,9 +209,8 @@ class HuffmanEncoder{
                 currentNode=decoder;
             }
         }
-        console.log("decoded string: ",res);
+        return res;
 
-        
         
     }
   
--- FileZipper/controller.js
+++ FileZipper/controller.js
@@ -1,5 +1,5 @@
 import {HuffmanEncoder} from "./Huffman.js";
-var onload= function(){
+window.onload= function(){
 // testing onload funtion 
 console.log("hello world!");
 const ebtn= document.getElementById("ebtn");
@@ -7,7 +7,7 @@ const dbtn= document.getElementById("dbtn");
 const ubtn= document.getElementById("ubtn");
 const upload= document.getElementById("upload");
 const download= document.getElementById("download");
-
+const span1= document.getElementById("span1");
 /// cant encode or decode till file is added
 ebtn.disabled=true;
 dbtn.disabled=true;
@@ -16,6 +16,8 @@ let uploaded_file_string="";
 let t2u="";
 // uploading function
 ubtn.onchange=function(){
+        upload.style.color="black";
+        download.style.color="black";
         ebtn.disabled=false;
         dbtn.disabled=false;
         console.log("file uploading!");
@@ -23,7 +25,7 @@ ubtn.onchange=function(){
         reader.readAsText(ubtn.files[0]);
         reader.onload=function(){
             t2u=reader.result;
-            console.log(t2u);
+            //console.log("input document string-",t2u);
             upload.innerHTML=t2u;
         }
 
@@ -31,11 +33,45 @@ ubtn.onchange=function(){
     }
 
 ebtn.onclick=()=>{
-
+ 
+    upload.style.fontWeight="bolder";
+    upload.style.display="flex";
+    upload.style.justifyContent="center";
+    upload.style.alignItems="center";
+    upload.style.color="black";
+    download.style.color="black";
+    upload.style.fontSize="bolder";
     let he= new HuffmanEncoder();
-    download.innerHTML(he.encode(t2u));
+    let encoding_info= he.encode(t2u);
+    download.innerHTML=encoding_info[0];
+    upload.innerHTML=encoding_info[1];
+    
+ 
+
+    downloadFile(ubtn.files[0]['name'].split('.')[0] +'_encoded.txt', encoding_info[0]);
     
 }
+
+dbtn.onclick=()=>{
+    let he= new HuffmanEncoder();
+    let decoded_info=he.decode(t2u); 
+    download.innerHTML=decoded_info;
+    window.alert("decoding in process!");
+    downloadFile(ubtn.files[0]['name'].split('.')[0]+'_decoded.txt',decoded_info);
+}
+
+
+
+
+function downloadFile(name,data)
+{
+    window.alert("Downloading File!");
+    let a = document.createElement('a');
+    a.href = "data:application/octet-stream,"+encodeURIComponent(data);
+    a.download = name;
+    a.click();
+};
+
 
 }
 
